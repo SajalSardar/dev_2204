977b4bd48bb14f6409eca9963551c50571b57876
A B M Shawon Islam <55059592+ABMshawonislam-dev@users.noreply.github.com>
2022-09-08 19:10:03 +0600

    Form Submit and image uploaded

---
+++ webdev_2204.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2022 at 03:03 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webdev_2204`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `photo` varchar(150) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `photo`, `status`) VALUES
+(1, 'Rahim', 'Islam', 'rahim@gmail.com', '202cb962ac59075b964b07152d234b70', 'photo.jpg', 1),
+(2, 'Karim', 'Islam', 'karim@gmail.com', '202cb962ac59075b964b07152d234b70', 'photo.jpg', 1),
+(4, 'James Dickson', 'Jenette Hendrix', 'luka@mailinator.com', 'Pa$$w0rd!', 'photo.jpg', 1),
+(5, 'Stacy Mullen', 'Claudia Allen', 'wiriwoj@mailinator.com', 'f3ed11bbdb94fd9ebdefbaf646ab94d3', 'photo.jpg', 1),
+(6, 'Hammett Bradford', 'Fiona Jones', 'jobab@mailinator.com', 'f3ed11bbdb94fd9ebdefbaf646ab94d3', 'Hammett Bradford-6319e77749796.jpg', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
